006dd46a0e5414569658ed5e5e6fb10649d715d9
Maverick <maverick.schirmer@gmail.com>
2019-12-01 14:34:30 -0800

    Added eat-da-burger code

---
+++ config/index.js
@@ -0,0 +1,7 @@
+module.exports = require('mysql2')
+.createConnection({
+  host:'g9fej9rujq0yt0cd.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user: 'jcmuxvzc97e6z7x7',
+  password: 'y3kh6gbmxvk41azo',
+  database: 'q9uj8uy73z1mm638'
+})
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const { join } = require('path')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.engine('.hbs', require('express-handlebars')({extname: '.hbs', defaultLayout: null }))
+app.set('view engine', '.hbs')
+
+require('./routes')(app)
+
+
+app.listen(process.env.PORT || 3000)
